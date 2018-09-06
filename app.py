b0dd62639a3d3c9e9d28830cea7649e5639a9175
Dan Uluma <danuluma@gmail.com>
2018-09-06 11:55:33 +0300

    added api endpoint to get a specific order

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import jsonify
+from flask import abort
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -18,6 +20,10 @@ orders = [
     }
 ]
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 @app.route('/')
 def index():
     return "Hi person ;-)"
@@ -25,6 +31,13 @@ def index():
 @app.route('/dann/api/v1.0/orders', methods=['GET'])
 def get_orders():
     return jsonify({'orders': orders})
+
+@app.route('/dann/api/v1.0/orders/<int:order_id>', methods=['GET'])
+def get_order(order_id):
+    order =[order for order in orders if order['id'] == order_id ]
+    if len(order)==0:
+        abort(404)
+    return jsonify({'order': order[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
