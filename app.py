ebcb15f60e33dbe73ea872742fbd19ed02f663ea
Dan Uluma <danuluma@gmail.com>
2018-09-06 11:31:52 +0300

    added api endpoint to get all orders

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
+
+orders = [
+    {
+    'id': 1,
+    'title': 'pancake',
+    'description':'Lorem ipsum',
+    'price': 50
+    },
+    {
+    'id': 2,
+    'title': 'pizza',
+    'description':'Lorem ipsum',
+    'price': 500
+    }
+]
 
 @app.route('/')
 def index():
     return "Hi person ;-)"
+
+@app.route('/dann/api/v1.0/orders', methods=['GET'])
+def get_orders():
+    return jsonify({'orders': orders})
 
 if __name__ == '__main__':
     app.run(debug=True)
