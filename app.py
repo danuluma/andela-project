00c14dba264b08abbea7b6df638d27edf6d26723
Dan Uluma <danuluma@gmail.com>
2018-09-06 11:58:59 +0300

    added api endpoint to place a new order

--- app.py
+++ app.py
@@ -39,5 +39,18 @@ def get_order(order_id):
         abort(404)
     return jsonify({'order': order[0]})
 
+@app.route('/dann/api/v1.0/orders', methods=['POST'])
+def create_order():
+    if not request.json or not 'title' in request.json:
+        abort(404)
+    order = {
+        'id': orders[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'price': 0
+    }
+    orders.append(order)
+    return jsonify({'order': order}), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
