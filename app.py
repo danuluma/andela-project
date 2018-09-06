e974c191a3056f6e23f890a6b40fca88a171156f
Dan Uluma <danuluma@gmail.com>
2018-09-06 12:07:08 +0300

    added api endpoint to update an order

--- app.py
+++ app.py
@@ -53,5 +53,21 @@ def create_order():
     orders.append(order)
     return jsonify({'order': order}), 201
 
+@app.route('/dann/api/v1.0/orders/<int:order_id>', methods=['PUT'])
+def update_order(order_id):
+    order =[order for order in orders if order['id'] == order_id ]
+    if len(order)==0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    order[0]['title'] = request.json.get('title', order[0]['title'])
+    order[0]['description'] = request.json.get('description', order[0]['description'])
+    order[0]['price'] = request.json.get('price', order[0]['price'])
+    return jsonify({'order': order[0]})
+
 if __name__ == '__main__':
     app.run(debug=True)
