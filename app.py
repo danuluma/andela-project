e8bf58ac33dbc7655d44c5f6d865ebb6beae5fbc
Dan Uluma <danuluma@gmail.com>
2018-09-07 22:08:38 +0300

    integrated pylint and refactored code according to lint specifications

--- app.py
+++ app.py
@@ -7,42 +7,47 @@ from flask import request
 app = Flask(__name__)
 
 orders = [
-    {
-    'id': 1,
-    'title': 'pancake',
-    'description':'Lorem ipsum',
-    'price': 50
-    },
-    {
-    'id': 2,
-    'title': 'pizza',
-    'description':'Lorem ipsum',
-    'price': 500
-    }
+ {
+  'id': 1,
+  'title': 'pancake',
+  'description': 'Lorem ipsum',
+  'price': 50
+ },
+ {
+  'id': 2,
+  'title': 'pizza',
+  'description': 'Lorem ipsum',
+  'price': 500
+ }
 ]
+
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 @app.route('/')
 def index():
     return "Hi person ;-)"
+
 
 @app.route('/dann/api/v1/orders', methods=['GET'])
 def get_orders():
     return jsonify({'orders': orders})
 
+
 @app.route('/dann/api/v1/orders/<int:order_id>', methods=['GET'])
 def get_order(order_id):
-    order =[order for order in orders if order['id'] == order_id ]
-    if len(order)==0:
+    order = [order for order in orders if order['id'] == order_id ]
+    if len(order) == 0:
         abort(404)
     return jsonify({'order': order[0]})
 
+
 @app.route('/dann/api/v1/orders', methods=['POST'])
 def create_order():
-    if not request.json or not 'title' in request.json:
+    if not request.json or'title' not in request.json:
         abort(404)
     order = {
         'id': orders[-1]['id'] + 1,
@@ -53,10 +58,11 @@ def create_order():
     orders.append(order)
     return jsonify({'order': order}), 201
 
+
 @app.route('/dann/api/v1/orders/<int:order_id>', methods=['PUT'])
 def update_order(order_id):
-    order =[order for order in orders if order['id'] == order_id ]
-    if len(order)==0:
+    order = [order for order in orders if order['id'] == order_id]
+    if len(order) == 0:
         abort(404)
     if not request.json:
         abort(400)
@@ -68,6 +74,7 @@ def update_order(order_id):
     order[0]['description'] = request.json.get('description', order[0]['description'])
     order[0]['price'] = request.json.get('price', order[0]['price'])
     return jsonify({'order': order[0]}), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- dann_test.py
+++ dann_test.py
@@ -2,6 +2,7 @@ import json
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client(request):
     test_client = app.test_client()
@@ -12,25 +13,31 @@ def client(request):
     request.addfinalizer(teardown)
     return test_client
 
+
 def post_json(client, url, data):
     """Send data as json to the specified url """
     return client.post(url, data=json.dumps(data), content_type='application/json')
+
 
 def put_json(client, url, data):
     """Send data as json to the specified url """
     return client.put(url, data=json.dumps(data), content_type='application/json')
 
+
 def json_of_response(response):
     """Decode json from response"""
     return json.loads(response.data.decode('utf8'))
+
 
 def test_home(client):
     response = client.get('/')
     assert b'Hi person ;-)' in response.data
 
+
 def test_get_all(client):
     response = client.get('/dann/api/v1/orders')
     assert response.status_code == 200
+
 
 def test_get_order1(client):
     response = client.get('/dann/api/v1/orders/1')
@@ -44,6 +51,7 @@ def test_create_order(client):
     assert response.status_code == 404
     response = post_json(client, '/dann/api/v1/orders', {'title': 'burger'})
     assert response.status_code == 201
+
 
 def test_edit_order(client):
     response = put_json(client, '/dann/api/v1.0/orders/1', {'': ''})
