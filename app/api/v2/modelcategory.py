755acde045d5e194766580654d6b08abdcffa216
Dan Uluma <danuluma@gmail.com>
2018-09-30 03:06:17 +0300

    [Chore #160753917] created category model

--- app/api/v2/modelcategory.py
+++ app/api/v2/modelcategory.py
@@ -5,25 +5,25 @@ sys.path.insert(0, LOCALPATH + '/../../../')
 from app.api.v2.db import Db1
 
 
-class OrderModel(Db1):
+class CategoryModel(Db1):
   """docstring for Menu"""
   def __init__(self):
     pass
 
-  def get_all_orders(self):
+  def get_all_categories(self):
     conn = Db1("DBASE").connect1()
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM orders""")
+    cur.execute("""SELECT * FROM categories""")
     rows = cur.fetchall()
     orders = []
     for row in rows:
       print(row)
-      item = {'id': row[0], 'price': row[1], 'description': row[2], 'ordered_by': row[3], 'order_date': row[4], 'status': row[5]}
+      item = {'id': row[0], 'name': row[1], 'description': row[2]}
       orders.append(item)
     print(orders)
     return orders
 
-  def get_single_order(self, order_id):
+  def get_single_category(self, category_id):
     conn = Db1("DBASE").connect1()
     cur = conn.cursor()
     cur.execute("""SELECT * FROM orders""")
@@ -31,43 +31,43 @@ class OrderModel(Db1):
     order = []
     for row in rows:
       print(row)
-      if row[0] == order_id :
-          item = {'id': row[0], 'price': row[1], 'description': row[2], 'ordered_by': row[3], 'order_date': row[4], 'status': row[5]}
+      if row[0] == category_id :
+          item = {'id': row[0], 'name': row[1], 'description': row[2]}
           order.append(item)
     print(order)
     return order
 
-  def add_new_order(self, order_details):
-    print(order_details)
+  def add_new_category(self, category_details):
+    print(category_details)
     insertsql = """
-    INSERT INTO orders (price, description, ordered_by, order_date, status)
-    VALUES (%s,%s,%s,%s,%s);
+    INSERT INTO categories (name, description)
+    VALUES (%s,%s);
     """
     conn = Db1("DBASE").connect1()
     cur = conn.cursor()
-    cur.execute(insertsql, order_details)
+    cur.execute(insertsql, category_details)
     conn.commit()
     print("2 insert success")
     return {"Suceesss":"SSSS"}
 
-  def update_order_details(self, order_id, orderdetails):
+  def update_category_details(self, category_id, categorydetails):
 
-    updatesql = """UPDATE orders SET price = %s, description = %s, ordered_by = %s, order_date = %s, status = %s WHERE id = {}""".format(order_id)
+    updatesql = """UPDATE categories SET name = %s, description = %s WHERE id = {}""".format(category_id)
     conn = Db1("DBASE").connect1()
     cur = conn.cursor()
-    cur.execute(updatesql, orderdetails)
+    cur.execute(updatesql, categorydetails)
     conn.commit()
-    selectsql = """SELECT * FROM orders WHERE id = {}""".format(order_id)
+    selectsql = """SELECT * FROM categories WHERE id = {}""".format(category_id)
     cur.execute(selectsql)
     record = cur.fetchone()
     print(record)
     return record
 
-  def delete_order(self, order_id):
+  def delete_category(self, category_id):
     conn = Db1("DBASE").connect1()
     cur = conn.cursor()
     print("I'm about to delete")
-    delsql = """DELETE FROM orders WHERE id = {0}""".format(order_id)
+    delsql = """DELETE FROM categories WHERE id = {}""".format(category_id)
     cur.execute(delsql)
     conn.commit()
 
