12dbdfba8bee749003848e87e4677ac9633df934
Dan Uluma <danuluma@gmail.com>
2018-10-02 09:58:47 +0300

    [Chore #160905461] removed redundant code

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_restful import Api
 
 
 # local imports
-# from instance.config import app_config
 from app.api.v1.views import Home, Orders, MyOrder
 from app.api.v1.auth import Reg, Login, Refresh
 from app.api.v2.views.auth import Signup, Loginv2
