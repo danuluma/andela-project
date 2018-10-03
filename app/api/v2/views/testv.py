260fcb70fcad81dcba258b361c0dec98271cfc62
Dan Uluma <danuluma@gmail.com>
2018-10-03 09:45:40 +0300

    [Feature #160905999] added methods to validate data

---
+++ app/api/v2/views/testv.py
@@ -0,0 +1,10 @@
+import os, sys
+LOCALPATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, LOCALPATH + '/../../../../')
+
+from app.api.v2.models.validate import Validate
+
+
+# print(Validate().validate_email("@dan.com"))
+print(Validate().validate_pass("wfduvbfehT6"))
+# print(Validate().validate_phone("5434346"))
