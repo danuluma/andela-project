65d4c329aad40b527776935c72d0dd5a7e95db94
Dan Uluma <danuluma@gmail.com>
2018-10-06 19:42:43 +0300

    [Ch #160935749] config

--- app/api/v2/views/view_order.py
+++ app/api/v2/views/view_order.py
@@ -1,6 +1,6 @@
 from flask import abort
 from flask import request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource, reqparse
 import os, sys
 
